c9c13b79091d93264223866016b53c3faa7f2d17
code.kraft <sen.arnab@gmail.com>
2024-04-04 08:55:24 +0200

    added fib with gen

---
+++ fib.py
@@ -0,0 +1,11 @@
+def genFib(num):
+  first = 0
+  second = 1
+  for _ in range(num):
+    yield first
+    temp = first + second
+    first = second
+    second = temp
+    
+for i in genFib(1000):
+  print (i)
